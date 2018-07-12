671f4af3e5900569008f38c49c24dc5203a83202
Itchakhun <supadech@dezainwerkz.com>
2018-07-12 18:12:50 +0700

    save

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import { GraphQLServer } from 'graphql-yoga';
-import { Query, Mutation } from './resolvers';
+const { GraphQLServer }  =require('graphql-yoga');
+const { Prisma }  =require('prisma-binding');
+const { Query, Mutation }  =require('./resolvers');
+
+const context = (req) => ({
+  ...req,
+  db: new Prisma({
+    typeDefs: './src/generated/prisma.graphql',
+    endpoint: 'https://eu1.prisma.sh/itchakhun-6f75a7/blopper-news/dev',
+    secret: 'legacy001',
+    debug: true,
+  })
+})
 
 const resolvers = {
   Query,
@@ -8,7 +19,8 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context
 });
 
 server.start(() => console.log('👉🏻  http://localhost:4000 👈🏻'));
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,45 +1,17 @@
-const links = [
-  {
-    id: 'LINK1',
-    description: 'asd',
-    url: 'www.sadasd.com'
-  }
-];
-
-export const Query = {
+const Query = {
   info: () => 'This is Blopper news',
-  feeds: () => links,
-  link: (_, { id }) => links.find(e => e.id === id)
+  feeds: (rot,arg,con,inf) => con.db.query.links({}, inf)
 };
 
-export const Mutation = {
-  post: (_, args) => {
-    const { description, url } = args;
-    const link = {
-      id: `LINK${links.length + 1}`,
-      description,
-      url
-    };
-    links.push(link);
-    return link;
+const Mutation = {
+  post: (_, arg, con, inf) => {
+    return con.db.createLink({
+      data: {
+        url: arg.url,
+        description: arg.description
+      }
+    }, inf)
   },
-  put: (_, args) => {
-    const { id } = args;
-    const index = links.findIndex(e => e.id === id);
-    if (index > -1) {
-      links[index] = args;
-      return links[index];
-    }
-    throw new Error('Fail to update');
-  },
-  delete: (_, args) => {
-    const { id } = args;
-    const index = links.findIndex(e => e.id === id);
-    if (index > -1) {
-      const remove = links[index];
-      links.splice(index, 1);
-      return remove;
-    }
-    throw new Error('Fail to update');
-  }
 };
+
+module.exports = {Query, Mutation}
